2bd8a60fdaf1745a58934eaac4b11d1bacc095b9
william.bumba <willian.bumba@tistech.co.ao>
2022-11-30 16:43:38 +0100

    client adicionado ao projecto

---
+++ fixinde-client/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './view/app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'fixinde';
+}
